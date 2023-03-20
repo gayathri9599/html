3ef1ab7059aefe4a8524ebbef9c0f722c47cdebd
R <X023204@ead.thomsonreuters.com>
2023-03-20 23:04:09 +0530

    fundamentals

--- fundas.js
+++ fundas.js
@@ -12,3 +12,23 @@ console.log(user_name);
 
 var result= true;
 console.log(result);
+
+// undefined
+
+var input_data;
+console.log(input_data);
+
+//object- example 1
+
+var student_info= {
+      name: "Gayathri",
+      qual: "Software dev", 
+      age: 23,
+      location: "Chennai" 
+    }
+console.log(student_info);
+
+// object example 2
+
+var employee_information={ name:"Gayathri", age: 23, location: "bangalore" , company:"HCLTech"}
+console.log(employee_information);
