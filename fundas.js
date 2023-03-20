4c889eac70c5a16c27475a490feeeb6d40f86034
R <X023204@ead.thomsonreuters.com>
2023-03-20 22:10:13 +0530

    fundamentals

---
+++ fundas.js
@@ -0,0 +1,14 @@
+// Number
+
+var user_age = 35;
+console.log(user_age);
+
+// string
+
+var user_name ="Gayathri Ramesh";
+console.log(user_name);
+
+// boolean
+
+var result= true;
+console.log(result);
--- program.js
+++
@@ -1,6 +0,0 @@
-var user_age=28;
-console.age(user_age);
-
-function function_name(){
-    
-}
